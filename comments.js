e05ef5163328ab22282725b3cb2121003b12ac2a
Sarthak <97225974+turingAlan@users.noreply.github.com>
2024-01-07 19:27:35 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,36 @@
-//create a server
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/comment_controller');
+// Create web server using express
+// The server will be used to serve comments from the database
 
-//route for creating comment
-router.post('/create',commentController.create);
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Comment = require('./model/comments');
 
-//route for deleting comment
-router.get('/destroy/:id',commentController.destroy);
+// MongoDB
+mongoose.connect('mongodb://localhost/comments');
 
-module.exports == router;
+// Express
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+// Routes
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// Get comments
+app.get('/api/comments', function(req, res) {
+  Comment.find(function(err, comments) {
+    if (err)
+      res.send(err);
+    res.json(comments);
+  });
+});
+
+// Post comments
+app.post('/api/comments', function(req, res) {
+  Comment.create({
+    text: req.body.text,})})
