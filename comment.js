1b9444b459572c7485c37bb00d07e5b6e1c32c6d
Sarthak <97225974+turingAlan@users.noreply.github.com>
2024-01-07 18:58:47 +0000

    fourth commit

--- comment.js
+++ comment.js
@@ -1,30 +1,11 @@
-// create a server with express
-const express = require('express');
-const app = express();
-// connect to mongodb
-const mongoose = require('mongoose');
-const { MONGOURI } = require('./keys');
-mongoose.connect(MONGOURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-mongoose.connection.on('connected', () => {
-    console.log('connected to mongodb');
-});
-mongoose.connection.on('error', (err) => {
-    console.log('error connecting', err);
-});
-// register models
-require('./models/user');
-require('./models/post');
-// use body parser for parsing json data
-app.use(express.json());
-// use routes
-app.use(require('./routes/auth'));
-app.use(require('./routes/post'));
-app.use(require('./routes/user'));
-// config port
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log('server is running on', PORT);
-});
+// create a server
+var http = require('http');
+var url = require('url');
+
+// create a server object:
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query; // parse the url
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(8080); // the server object listens on port 8080
